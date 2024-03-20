0bf48180b204141fa41125b36371abd2c716c190
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-03-21 00:20:00 +0800

    fix: add flavors and addOns relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Builder;
@@ -45,4 +46,11 @@ class Product extends Model
         return $this->belongsTo(Category::class, 'category_id');
     }
 
+    public function flavors(): BelongsToMany {
+        return $this->belongsToMany(Flavor::class);
+    }
+
+    public function addOns(): BelongsToMany {
+        return $this->belongsToMany(AddOn::class);
+    }
 }
