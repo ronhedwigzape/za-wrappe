7a1174861b2b07401dc0e024c156d4ff5f82d445
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-03-27 16:49:34 +0800

    fix: add missing attribute for `order_items` table

--- database/migrations/2024_03_14_112307_create_order_items_table.php
+++ database/migrations/2024_03_14_112307_create_order_items_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained();
             $table->integer('quantity');
             $table->text('customizations')->nullable();
+            $table->float('subtotal');
             $table->timestamps();
         });
     }
