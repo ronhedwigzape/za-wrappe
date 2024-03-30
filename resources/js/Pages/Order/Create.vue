c7783a9d68f4bbf59ba5cfbfa1346b82b7c75f44
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-03-30 13:01:18 +0800

    chore: optimize order store and order creation component

--- resources/js/Pages/Order/Create.vue
+++ resources/js/Pages/Order/Create.vue
@@ -2,10 +2,10 @@
     <div>
         <div class="max-w-4xl mx-auto p-4">
             <!-- Category Selection -->
-            <div v-if="!productToCustomize && orderStore.ordering" class="space-y-4">
+            <div v-if="!orderStore.productToCustomize && orderStore.ordering" class="space-y-4">
                 <h2 class="text-2xl font-semibold">Select a Category</h2>
                 <div class="flex flex-wrap">
-                    <button v-for="category in orderStore.categories" :key="category.id" @click="selectProducts(category)"
+                    <button v-for="category in orderStore.categories" :key="category.id" @click="orderStore.selectProducts(category)"
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1">
                         {{ category.name }}
                     </button>
@@ -13,46 +13,46 @@
             </div>
 
             <!-- Product Selection -->
-            <div v-if="selectedCategory && !productToCustomize" class="space-y-4">
-                <h2 class="text-2xl font-semibold">Select a Product from {{ selectedCategory.name }}</h2>
+            <div v-if="orderStore.selectedCategory && !orderStore.productToCustomize" class="space-y-4">
+                <h2 class="text-2xl font-semibold">Select a Product from {{ orderStore.selectedCategory.name }}</h2>
                 <div class="flex flex-wrap">
-                    <button v-for="product in selectedCategoryProducts" :key="product.id" @click="selectProduct(product)"
+                    <button v-for="product in selectedCategoryProducts" :key="product.id" @click="orderStore.selectProduct(product)"
                             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-1">
                         {{ product.name }} - ₱{{ product.price }}
                     </button>
                 </div>
-                <button @click="goBackToCategories"
+                <button @click="orderStore.goBackToCategories"
                         class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
                     Back to Categories
                 </button>
             </div>
 
             <!-- Product Customization: Flavor, Size or Type Selection -->
-            <div v-if="productToCustomize && isCustomizingFlavorAndAddOns" class="space-y-4">
-                <h2 class="text-2xl font-semibold">Customize Your {{ productToCustomize.name }}</h2>
+            <div v-if="orderStore.productToCustomize && orderStore.isCustomizingFlavorAndAddOns" class="space-y-4">
+                <h2 class="text-2xl font-semibold">Customize Your {{ orderStore.productToCustomize.name }}</h2>
                 <div class="flex flex-wrap">
-                    <h3 class="text-xl font-semibold">Current Price: ₱{{ currentPrice.toFixed(2) }}</h3>
+                    <h3 class="text-xl font-semibold">Current Price: ₱{{ orderStore.currentPrice.toFixed(2) }}</h3>
 
                     <!-- Quantity Selection -->
                     <div class="w-full space-y-2">
                         <h3 class="text-lg font-medium">Quantity</h3>
-                        <input type="number" v-model.number="quantity" min="1" class="border-2 border-gray-300 rounded py-2 px-4" />
+                        <input type="number" v-model.number="orderStore.quantity" min="1" class="border-2 border-gray-300 rounded py-2 px-4" />
                     </div>
 
                     <!-- Display currently selected flavor -->
-                    <div v-if="selectedFlavor" class="w-full mb-4">
-                        <span class="text-lg font-medium">Selected Flavor: </span>{{ selectedFlavor.name }}
+                    <div v-if="orderStore.selectedFlavor" class="w-full mb-4">
+                        <span class="text-lg font-medium">Selected Flavor: </span>{{ orderStore.selectedFlavor.name }}
                     </div>
 
                     <div v-if="selectedFlavors.length" class="w-full space-y-2">
                         <h3 class="text-lg font-medium">Select Flavor</h3>
                         <div class="flex flex-wrap">
                             <button
-                                v-for="flavor in selectedFlavors" :key="flavor.id" @click="selectFlavor(flavor)"
+                                v-for="flavor in selectedFlavors" :key="flavor.id" @click="orderStore.selectFlavor(flavor)"
                                 class="hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded m-1"
                                 :class="{
-                                    'bg-indigo-700': selectedFlavor && selectedFlavor.id === flavor.id,
-                                    'bg-indigo-500': !(selectedFlavor && selectedFlavor.id === flavor.id)
+                                    'bg-indigo-700': orderStore.selectedFlavor && orderStore.selectedFlavor.id === flavor.id,
+                                    'bg-indigo-500': !(orderStore.selectedFlavor && orderStore.selectedFlavor.id === flavor.id)
                                 } ">
                                 {{ flavor.name }}
                             </button>
@@ -62,21 +62,21 @@
                     <div v-if="selectedAddOnsList.length" class="space-y-2">
                         <h3 class="text-lg font-medium">Add Ons</h3>
                         <div class="flex flex-wrap">
-                            <button v-for="addOn in selectedAddOnsList" :key="addOn.id" @click="toggleAddOn(addOn)"
+                            <button v-for="addOn in selectedAddOnsList" :key="addOn.id" @click="orderStore.toggleAddOn(addOn)"
                                     class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-1">
                                 {{ addOn.name }} (+₱{{ addOn.price }})
                             </button>
                         </div>
                     </div>
 
-                    <button v-if="!isCartUpdating" @click="goBackToProducts"
+                    <button v-if="!orderStore.isCartUpdating" @click="orderStore.goBackToProducts"
                             class="mt-4 bg-gray-200 hover:bg-blue-800 text-black font-bold py-2 px-4 rounded">
                         Go Back to Products
                     </button>
 
-                    <button @click="finalizeCustomization()"
+                    <button @click="orderStore.finalizeCustomization()"
                             class="mt-4 bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
-                        {{ isCartUpdating ? "Update item to cart" : "Add to cart" }}
+                        {{ orderStore.isCartUpdating ? "Update item to cart" : "Add to cart" }}
                     </button>
                 </div>
             </div>
@@ -85,7 +85,7 @@
             <div v-if="orderStore.cart.length && orderStore.ordering" class="mt-8 space-y-4">
                 <h2 class="text-2xl font-semibold">Your Cart</h2>
                 <ul class="list-disc pl-5">
-                    <li v-for="(item, index) in orderStore.cart" :key="index" @click="customizeCartItem(item.id)" class="cursor-pointer mt-2">
+                    <li v-for="(item, index) in orderStore.cart" :key="index" @click="orderStore.customizeCartItem(item.id)" class="cursor-pointer mt-2">
                     <div class="font-medium">{{ item.name }}</div>
 
                         <div v-if="item.flavor">{{ item.flavor }}</div>
@@ -109,7 +109,7 @@
                 <div class="font-bold">Total: ₱{{ parseFloat(orderStore.cartTotal).toFixed(2) }}</div>
 
                 <div class="flex space-x-4">
-                    <button @click="continueOrdering"
+                    <button @click="orderStore.continueOrdering"
                             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                         Continue Ordering
                     </button>
@@ -118,7 +118,7 @@
                             Review Order
                         </button>
                     </Link>
-                    <button @click="cancelOrder" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    <button @click="orderStore.cancelOrder" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                         Cancel Order
                     </button>
                 </div>
@@ -129,144 +129,18 @@
 
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/vue3';
-import { ref, computed, onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useOrderStore } from '@/Store/store-order';
 
 // store
 const orderStore = useOrderStore();
-
-// data
-const selectedCategory = ref(null);
-const productToCustomize = ref(null);
-const selectedFlavor = ref(null);
-const selectedAddOns = ref([]);
-const isCustomizingFlavorAndAddOns = ref(false);
-const quantity = ref(1);
-const customizations = ref("");
-const isCartUpdating = ref(false);
 
 // computed
 const selectedCategoryProducts = computed(() => orderStore.products);
 const selectedFlavors = computed(() => orderStore.categorySpecificFlavors);
 const selectedAddOnsList = computed(() => orderStore.categorySpecificAddOns);
 
-const currentPrice = computed(() => {
-    let basePrice = productToCustomize.value ? parseFloat(productToCustomize.value.price) : 0;
-    let flavorPrice = selectedFlavor.value && selectedFlavor.value.price ? parseFloat(selectedFlavor.value.price) : 0;
-    let addOnsPrice = selectedAddOns.value && selectedAddOns.value.length > 0
-        ? selectedAddOns.value.reduce((sum, addOn) => sum + parseFloat(addOn.price || 0), 0)
-        : 0;
-    let totalPriceWithoutQuantity = basePrice + flavorPrice + addOnsPrice;
-    let totalPrice = totalPriceWithoutQuantity * quantity.value;
-    return parseFloat(totalPrice.toFixed(2));
-});
-
-// methods
-function finalizeCustomization() {
-    const cartItem = {
-        id: productToCustomize.value.id,
-        ...productToCustomize.value,
-        variant: productToCustomize.value,
-        selectedFlavorId: selectedFlavor.value ? selectedFlavor.value.id : null,
-        selectedAddOnsIds: selectedAddOns.value.map(addOn => addOn.id),
-        flavor: selectedFlavor.value ? selectedFlavor.value.name : 'None',
-        addOns: selectedAddOns.value,
-        currentPrice: currentPrice.value,
-        quantity: quantity.value,
-        customizations: customizations.value,
-    };
-    if (isCartUpdating.value) {
-        orderStore.updateCartItem(cartItem);
-    } else {
-        orderStore.addToCart(cartItem);
-    }
-    resetSelections();
-}
-
-async function customizeCartItem(itemId) {
-    const cartItem = orderStore.cart.find(item => item.id === itemId);
-    if (cartItem) {
-        try {
-            await orderStore.fetchCategorySpecificFlavors(cartItem.category_id);
-            await orderStore.fetchCategorySpecificAddOns(cartItem.category_id);
-            productToCustomize.value = { ...cartItem };
-            selectedFlavor.value = cartItem.selectedFlavor;
-            selectedAddOns.value = cartItem.addOns.map(addOn => {
-                return orderStore.categorySpecificAddOns.find(a => a.id === addOn.id);
-            }).filter(Boolean);
-            quantity.value = cartItem.quantity;
-            isCartUpdating.value = true;
-        } catch (e) {
-            console.error(e.toString());
-        }
-    }
-}
-
-async function selectProducts(category) {
-    selectedCategory.value = category;
-    try {
-        await orderStore.fetchProducts(category.id);
-        await orderStore.fetchCategorySpecificFlavors(category.id);
-        await orderStore.fetchCategorySpecificAddOns(category.id);
-    } catch (e) {
-        console.error(e.toString());
-    }
-}
-
-function selectProduct(product) {
-    productToCustomize.value = product;
-    isCustomizingFlavorAndAddOns.value = true;
-}
-
-function selectFlavor(flavor) {
-    if (selectedFlavor.value && selectedFlavor.value.id === flavor.id) {
-        selectedFlavor.value = null;
-    } else {
-        selectedFlavor.value = flavor;
-    }
-}
-
-function toggleAddOn(addOn) {
-    console.log(`Toggling add on: ₱{addOn.id} ₱{selectedAddOns.value}`)
-    const index = selectedAddOns.value.findIndex(a => a.id === addOn.id);
-    index > -1 ? selectedAddOns.value.splice(index, 1) : selectedAddOns.value.push(addOn);
-}
-
-function continueOrdering() {
-    orderStore.ordering = true;
-    resetSelections();
-}
-
-function cancelOrder() {
-    orderStore.clearCart();
-    orderStore.ordering = true;
-}
-
-function resetSelections() {
-    productToCustomize.value = null;
-    selectedFlavor.value = null;
-    selectedAddOns.value = [];
-    selectedCategory.value = null;
-    quantity.value = 1;
-    isCartUpdating.value = false;
-}
-
-function goBackToCategories() {
-    selectedCategory.value = null;
-    productToCustomize.value = null;
-}
-
-function goBackToProducts() {
-    if (selectedCategory.value && !selectedCategoryProducts.value.length) {
-        selectProducts(selectedCategory.value);
-    }
-    productToCustomize.value = null;
-    selectedFlavor.value = null;
-    selectedAddOns.value = [];
-    quantity.value = 1;
-    customizations.value = "";
-}
-
+// onMounted
 onMounted(async () => {
     if (!orderStore.categories.length) await orderStore.fetchCategories();
 });
--- resources/js/Store/store-order.js
+++ resources/js/Store/store-order.js
@@ -9,6 +9,14 @@ export const useOrderStore = defineStore('order', {
         ordering: true,
         categorySpecificFlavors: [],
         categorySpecificAddOns: [],
+        selectedCategory: null,
+        productToCustomize: null,
+        selectedFlavor: null,
+        selectedAddOns: [],
+        isCustomizingFlavorAndAddOns: false,
+        quantity: 1,
+        customizations: "",
+        isCartUpdating: false,
     }),
     getters: {
         cartTotal: (state) => {
@@ -29,6 +37,19 @@ export const useOrderStore = defineStore('order', {
                 itemTotal *= itemQuantity;
                 return total + itemTotal;
             }, 0);
+        },
+        selectedCategoryProducts: (state) => state.products,
+        selectedFlavors: (state) => state.categorySpecificFlavors,
+        selectedAddOnsList: (state) => state.categorySpecificAddOns,
+        currentPrice: (state) => {
+            let basePrice = state.productToCustomize ? parseFloat(state.productToCustomize.price) : 0;
+            let flavorPrice = state.selectedFlavor && state.selectedFlavor.price ? parseFloat(state.selectedFlavor.price) : 0;
+            let addOnsPrice = state.selectedAddOns && state.selectedAddOns.length > 0
+                ? state.selectedAddOns.reduce((sum, addOn) => sum + parseFloat(addOn.price || 0), 0)
+                : 0;
+            let totalPriceWithoutQuantity = basePrice + flavorPrice + addOnsPrice;
+            let totalPrice = totalPriceWithoutQuantity * state.quantity;
+            return parseFloat(totalPrice.toFixed(2));
         },
     },
     actions: {
@@ -54,6 +75,24 @@ export const useOrderStore = defineStore('order', {
                 this.categorySpecificAddOns = response.data;
             } catch (error) {
                 console.log('Error fetching category-specific add-ons:', error);
+            }
+        },
+        goBackToCategories() {
+            this.selectedCategory = null;
+            this.productToCustomize = null;
+        },
+        async goBackToProducts() {
+            if (this.selectedCategory && !this.selectedCategoryProducts.length) {
+                try {
+                    await this.selectProducts(this.selectedCategory);
+                    this.productToCustomize = null;
+                    this.selectedFlavor = null;
+                    this.selectedAddOns = [];
+                    this.quantity = 1;
+                    this.customizations = "";
+                } catch (e) {
+                    console.error(e.toString())
+                }
             }
         },
         addToCart(product) {
@@ -88,5 +127,89 @@ export const useOrderStore = defineStore('order', {
         clearCart() {
             this.cart = [];
         },
+        async selectProducts(category) {
+            this.selectedCategory = category;
+            try {
+                await this.fetchProducts(category.id);
+                await this.fetchCategorySpecificFlavors(category.id);
+                await this.fetchCategorySpecificAddOns(category.id);
+            } catch (e) {
+                console.error(e.toString());
+            }
+        },
+        selectProduct(product) {
+            this.productToCustomize = product;
+            this.isCustomizingFlavorAndAddOns = true;
+        },
+        selectFlavor(flavor) {
+            if (this.selectedFlavor && this.selectedFlavor.id === flavor.id) {
+                this.selectedFlavor = null;
+            } else {
+                this.selectedFlavor = flavor;
+            }
+        },
+        toggleAddOn(addOn) {
+            console.log(`Toggling add on: AddOn ID:${addOn.id}, Add Ons:${this.selectedAddOns}`)
+            const index = this.selectedAddOns.findIndex(a => a.id === addOn.id);
+            if (index > -1) {
+                this.selectedAddOns.splice(index, 1);
+            } else {
+                this.selectedAddOns.push(addOn);
+            }
+        },
+        finalizeCustomization() {
+            const cartItem = {
+                id: this.productToCustomize.id,
+                ...this.productToCustomize,
+                variant: this.productToCustomize,
+                selectedFlavorId: this.selectedFlavor ? this.selectedFlavor.id : null,
+                selectedAddOnsIds: this.selectedAddOns.map(addOn => addOn.id),
+                flavor: this.selectedFlavor ? this.selectedFlavor.name : 'None',
+                addOns: this.selectedAddOns,
+                currentPrice: this.currentPrice,
+                quantity: this.quantity,
+                customizations: this.customizations,
+            };
+            if (this.isCartUpdating) {
+                this.updateCartItem(cartItem);
+            } else {
+                this.addToCart(cartItem);
+            }
+            this.resetSelections();
+        },
+        resetSelections() {
+            this.productToCustomize = null;
+            this.selectedFlavor = null;
+            this.selectedAddOns = [];
+            this.selectedCategory = null;
+            this.quantity = 1;
+            this.isCartUpdating = false;
+        },
+        async customizeCartItem(itemId) {
+            const cartItem = this.cart.find(item => item.id === itemId);
+            if (cartItem) {
+                try {
+                    await this.fetchCategorySpecificFlavors(cartItem.category_id);
+                    await this.fetchCategorySpecificAddOns(cartItem.category_id);
+                    this.productToCustomize = { ...cartItem };
+                    this.selectedFlavor = cartItem.selectedFlavor;
+                    this.selectedAddOns = cartItem.addOns.map(addOn => {
+                        return this.categorySpecificAddOns.find(a => a.id === addOn.id);
+                    }).filter(Boolean);
+                    this.quantity = cartItem.quantity;
+                    this.isCartUpdating = true;
+                } catch (e) {
+                    console.error(e.toString());
+                }
+            }
+        },
+        continueOrdering() {
+            this.ordering = true;
+            this.resetSelections();
+        },
+        cancelOrder() {
+            this.clearCart();
+            this.ordering = true;
+        }
     }
 });
