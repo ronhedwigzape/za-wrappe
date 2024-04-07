52eba714bab6718326545b067119243dc8912e84
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-04-07 18:48:12 +0800

    feat(routes/api.php): add necessary routes for the ordering kiosk

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\AddOnController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\FlavorController;
+use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\HelpController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -52,3 +54,41 @@ Route::post('/order/update', [OrderController::class, 'updateOrder']);
 // Clear cart (cancel order)
 Route::post('/order/cancel', [OrderController::class, 'cancelOrder']);
 
+// Define a route to fetch an order by ID
+Route::get('/orders/{orderId}', [OrderController::class, 'fetchOrder']);
+
+// Set Awaiting Payment Status
+Route::post('/orders/{order}/awaiting-payment', [OrderController::class, 'setAwaitingPayment'])->name('orders.awaiting-payment');
+
+// Set payment confirmation status
+Route::post('/payments/confirm', [OrderController::class, 'confirmPayment'])->name('payments.confirm');
+
+// Create Order
+Route::post('/orders/create', [OrderController::class, 'createOrder']);
+
+// Initialize or retrieve an existing order for the customer
+Route::post('/orders/init', [OrderController::class, 'initializeOrder']);
+
+// Add item to order (part of Order Creation step)
+Route::post('/orders/{order}/addItem', [OrderController::class, 'addItemToOrder']);
+
+// Manage Order Preparation - Fetch orders ready for preparation
+Route::get('/orders/ready-for-preparation', [OrderController::class, 'ordersReadyForPreparation']);
+
+// Update individual order item status by kitchen staff
+Route::post('/order-items/{orderItem}/update-status', [OrderController::class, 'updateOrderItemStatus']);
+
+// Update Order Pickup Status - Mark order as ready for pickup
+Route::post('/orders/{order}/ready-for-pickup', [OrderController::class, 'setOrderReadyForPickup']);
+
+Route::resource('/users', UserController::class);
+
+Route::resource('/roles', RoleController::class);
+
+// Feedbacks
+Route::get('/feedbacks', [FeedbackController::class, 'index']);
+Route::post('/feedbacks/respond', [FeedbackController::class, 'respondToFeedback']);
+
+// Reporting
+Route::get('/reports/sales', [ReportController::class, 'salesReport']);
+Route::get('/reports/financial', [ReportController::class, 'financialReport']);
