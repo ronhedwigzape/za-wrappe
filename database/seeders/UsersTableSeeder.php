b9a5b9f7237a51bbaebf68f57c678e721086ed1e
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-04-10 12:25:56 +0800

    feat: add initial data for users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,7 +16,32 @@ class UsersTableSeeder extends Seeder
     {
         // User::factory()->count(10)->create();
 
-        // Assuming 'Customer' role ID is 3
-        User::factory(10)->create(['role_id' => 3]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@zawrappe.com',
+            'password' => Hash::make('admin'),
+            'role_id' => 1,
+        ]);
+
+        User::create([
+            'name' => 'Merchant 1',
+            'email' => 'merchant1@zawrappe.com',
+            'password' => Hash::make('merchant1'),
+            'role_id' => 2,
+        ]);
+
+        User::create([
+            'name' => 'Merchant 2',
+            'email' => 'merchant2@zawrappe.com',
+            'password' => Hash::make('merchant2'),
+            'role_id' => 2,
+        ]);
+
+        User::create([
+            'name' => 'Customer',
+            'email' => 'customer@zawrappe.com',
+            'password' => Hash::make('customer'),
+            'role_id' => 3,
+        ]);
     }
 }
