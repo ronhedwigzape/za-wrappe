9ac7926f43423959f74e76e3325cf02d213de3f4
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-04-24 14:37:55 +0800

    wip(deployment): add necessary code for deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+// Use public_html as public (For Deployment)
+//$app->usePublicPath($app->basePath('public'));
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         vue({
@@ -17,4 +17,16 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        strictPort: true,
+        host: 'localhost'
+    },
+    build: {
+        outDir: 'public/build',
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
