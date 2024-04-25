dad5616aac507a4b631736c0d60aec4c32350478
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-04-25 10:18:18 +0800

    feat: Create ZaWrappeLogo component

---
+++ src/components/logo/ZaWrappeLogo.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <div class="flex flex-col items-center justify-center">
+        <img src="/assets/img/za_wrappe_logo.png" alt="za-wrappe-logo" style="height: 13em">
+    </div>
+</template>
