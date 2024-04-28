8dd47b9de424c8997d48ecc62dae54a149d21e31
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-04-28 16:09:00 +0800

    build: add storefrontui-vue to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { tailwindConfig } from '@storefront-ui/vue/tailwind-config';
 export default {
   prefix: 'tw-',
+  presets: [tailwindConfig],
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
     './**/*.vue',
+    './node_modules/@storefront-ui/vue/**/*.{js,mjs}'
   ],
   theme: {
     extend: {},
