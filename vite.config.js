e7c7857458a7fbba360e3d38291f5e3fd998d8d7
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-05-11 09:54:00 +0800

    refactor: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
-import vuetify from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
-  const config = {
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const SKIP_BASE_PATH = process.env.VITE_SKIP_BASE_PATH;
+
+  return defineConfig({
     plugins: [
       vue(),
       VueDevTools(),
       vuetify({
         autoImport: true
-      })
+      }),
+      splitVendorChunkPlugin() // Automatically split vendor modules into a separate chunk
     ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    base: undefined,
+    base: SKIP_BASE_PATH === undefined || SKIP_BASE_PATH === 'false' ? '/za-wrappe/' : undefined,
     publicDir: 'assets',
     server: {
       host: 'localhost',
@@ -28,14 +31,18 @@ export default ({mode}) => {
       strictPort: true
     },
     build: {
-      outDir: 'public'
+      outDir: 'public',
+      chunkSizeWarningLimit: 1600, // Increase the limit to 1600 KB to reduce warnings
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // Split node_modules into a separate chunk
+              return id.split('node_modules/')[1].split('/')[0];
+            }
+          }
+        }
+      }
     }
-  };
-
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  const SKIP_BASE_PATH = process.env.VITE_SKIP_BASE_PATH;
-  if (SKIP_BASE_PATH === undefined || SKIP_BASE_PATH === 'false')
-    config.base = '/za-wrappe/';
-
-  return defineConfig(config);
+  });
 };
